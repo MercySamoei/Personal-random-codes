1187621e92e2955d0728f539df65771f4951d07d
Mercy Samoei <59794245+MercySamoei@users.noreply.github.com>
2023-04-14 17:10:15 +0300

    Add files via upload

---
+++ least.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# read in the data
+df = pd.read_csv('museum_visitors.csv')
+
+# loop through each museum
+for museum in ['Avila Adobe', 'Firehouse Museum', 'Chinese American Museum', 'America Tropical Interpretive Center']:
+    
+    # group the data by date and sum the visitors for the current museum
+    grouped = df.groupby('Date')[museum].sum()
+    
+    top_5 = grouped.sort_values(ascending=True)[:5]
+    
+    # plot a bar chart for the top 5 days
+    plt.bar(top_5.index, top_5.values)
+    plt.xticks(rotation=90)
+    plt.ylabel('Number of visitors')
+    plt.title(f"Top 5 days when '{museum}' had the least visitors")
+    
+    # show the plot
+    plt.show()

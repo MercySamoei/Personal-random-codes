ae36d57c5136e7e11514e939de1c4b75c0de0d47
Mercy Samoei <59794245+MercySamoei@users.noreply.github.com>
2023-04-22 22:08:26 +0300

    Add files via upload

---
+++ pracfive.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import plotly.express as px
+df = px.data.carshare()
+print(df.head(10))
+print(df.tail(10))
+
+fig = px.scatter_mapbox(df,
+                        lon = df['centroid_lon'],
+                        lat = df['centroid_lat'],
+                        zoom = 3,
+                        color=df['peak_hour'],
+                        size = df['car_hours'],
+                        width = 1200,
+                        height = 900,
+                        title='Car Share Scatter Map'
+                        )
+fig.update_layout(mapbox_style = "open-street-map")
+fig.update_layout(margin={"r":0,"t":50,"l":0,"b":10})
